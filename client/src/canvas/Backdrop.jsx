6be37475a81e73bc9ccc69303cf9c98307c10e52
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-07-27 22:39:18 +0530

    fixing frontend deployment

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useRef } from "react";
-import { easing } from "maath";
+// import { easing } from "maath";
 import { useFrame } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
