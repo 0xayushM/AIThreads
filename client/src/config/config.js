0cc7cc0243217cbdc7591ba10b29809056c7f1a5
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-07-27 19:37:33 +0530

    frontend done

---
+++ client/src/config/config.js
@@ -0,0 +1,12 @@
+const config = {
+  development: {
+    backendUrl: "http://localhost:8080/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
+
+//place where we can put the links for the backend
